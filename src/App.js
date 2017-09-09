01db81b6c951a77dc681c4740c73d9ab1ae8da1d
ankit <ankit.j.arora@gmail.com>
2017-09-10 00:50:31 +0530

    add BookShelf and SearchBox UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link } from 'react-router-dom';
+import SearchBox from './SearchBox';
+import BookShelf from './BookShelf';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="app">
+                <Route
+                    exact
+                    path='/'
+                    render={() => (
+                        <div className="list-books">
+                            <div className="list-books-title">
+                                <h1>MyReads</h1>
+                            </div>
+                            <div className="list-books-content">
+                                <div>
+                                    <BookShelf />
+                                    <BookShelf />
+                                    <BookShelf />
+                                </div>
+                            </div>
+                            <div className="open-search">
+                                <Link to='/search'>Add a book</Link>
+                            </div>
+                        </div>
+                    )}
+                />
+                <Route
+                    path='/search'
+                    component={SearchBox}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
